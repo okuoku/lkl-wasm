4d2cc3e1c80315fae97238aa366ef08cc9b0716f
Yuki Okumura <mjt@cltn.org>
2023-11-04 16:51:10 +0900

    Update test code

--- _hostwasm/testlibc.c
+++ _hostwasm/testlibc.c
@@ -1,7 +1,14 @@
 #include <stdio.h>
+#include <unistd.h>
 
 int
 __original_main(int ac, char** av, char** envp){
+    int count = 0;
     printf("Hello, world!\n");
+    for(;;){
+        printf("Sleep...%d\n", count);
+        count++;
+        sleep(1);
+    }
     return 0;
 }
